ee0d78125d03202d2459b454958c91e54490e0d1
amardkum <amardkum@cisco.com>
2019-01-06 22:50:20 -0800

    file to hold env variables

---
+++ env_variables.py
@@ -0,0 +1,34 @@
+#!/usr/bin/python
+
+"""
+------------------------------- env_variables.py ------------------------------
+Description : This file is meant to maintain a dictionary of env. variables
+              along with their values as key-value pairs.
+
+Author: Amardeep Kumar <amardkum@cisco.com>; January 3rd, 2019
+
+Copyright (c) 2019 CISCO Systems.
+All rights reserved.
+-------------------------------------------------------------------------------
+"""
+import os
+
+env_variables = {
+    "SQS_URL": "https://sqs.us-east-1.amazonaws.com/283603838660/devCSBRelayQueueCAE.fifo",
+    "SQS_MSG_VISIBILITY_TIMEOUT": "600",
+    "CSB_CNT_REPO": "https://wwwin-github.cisco.com/CiscoIT-CSB/CiscoIT-CSB",
+    "CLONED_REPO_DIR": os.path.expanduser("~") + "/" + "csb_cnt_repo/",
+    "OS_INTERFACE": "public",
+    "OS_IDENTITY_API_VERSION": "3",
+    "OS_PROJECT_DOMAIN_NAME": "cisco",
+    "OS_USER_DOMAIN_NAME": "cisco",
+    "MAX_LIMIT_OF_SPAWNED_CONTAINER": "20",
+    "BREAKOUT_HOUR": "10",   # 1000 hrs.
+    "AWS_REGION": "us-east-1",
+    "ENV_TYPE": "",
+    "COUNT_OF_SQS_READ_MSG": "10"
+}
+
+if __name__ == "__main__":
+    print(env_variables)
+    pass
