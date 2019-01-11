42744d48a93f804d0982695c4eb3ae059b8754dc
amardkum <amardkum@cisco.com>
2019-01-11 14:44:40 -0800

    Update csb_auditor.py

--- csb_auditor.py
+++ csb_auditor.py
@@ -3,15 +3,15 @@
 """
 ---------------------------- csb_auditor.py ------------------------------
 Description: This python script is to
-			 a. decrypt the requried encrypted file(s)
-             b. clone git repo
-             c. create connection handle to SQS Queue
-             d. read messages from SQS Queue created for CNT-CSB
-             e. process the required information
-             f. pass the information to audit test-script
-             g. consolidate results from each audit test-script per team
-             h. post the result to kinesis
-             i. delete message from SQS Queue
+                a. decrypt the required encrypted file(s)
+                b. clone git repo
+                c. create connection handle to SQS Queue
+                d. read messages from SQS Queue created for CNT-CSB
+                e. process the required information
+                f. pass the information to audit test-script
+                g. consolidate results from each audit test-script per team
+                h. post the result to kinesis
+                i. delete message from SQS Queue
 
 Dependencies:
     csb_credentials.py.enc
@@ -29,30 +29,28 @@ All rights reserved.
 import boto3
 import botocore
 import git
-import json
 import multiprocessing
 import os
 import shutil
 import struct
-import sys
 import time
 
 from Crypto.Cipher import AES
 from env_variables import env_variables
-from general_util import add_result_to_stream, updateScanRecord, send_result_complete
 from importlib import import_module
 
 
-
-def decrypt_file(key, in_filename):
+def decrypt_file(in_filename):
     """
     Decrypts a file using AES(CBC mode) with the given key.
-    :param key: key to be used for decryption
     :param in_filename: name of encrypted file
-    :return: generated decrypted file and return TRUE/FALSE
+    :return: generate decrypted file and return TRUE/FALSE
     """
+    """ Key to decrypt the encrypted files required for CSB Audit """
+    key = "1329ebbc1b9646b890202384beaef2ec"
     out_filename = os.path.splitext(in_filename)[0]
     chunk_size = 64*1024
+
     try:
         print("LOG: Decrypt %s file" % in_filename)
         with open(in_filename, 'rb') as infile:
@@ -108,37 +106,23 @@ def read_message_from_sqs(sqsclient):
     :param sqsclient: sqs handle
     :return: batch of message(s) received from SQS Queue or None
     """
-    response = None
+    print("INFO: Read message from SQS")
     try:
-        print("INFO: Read message from SQS")
-        while True:
-            try:
-                response = sqsclient.receive_message(
-                                                        QueueUrl=os.environ["SQS_URL"],
-                                                        AttributeNames=[
-                                                            'All'
-                                                        ],
-                                                        MaxNumberOfMessages=int(os.environ["COUNT_OF_SQS_READ_MSG"]),
-                                                        MessageAttributeNames=[
-                                                            'All'
-                                                        ],
-                                                        VisibilityTimeout=int(os.environ["SQS_MSG_VISIBILITY_TIMEOUT"])
-                                                    )
-                if response.get("Messages", None):
-                    break
-                else:
-                   print("INFO: Attempt to read message from SQS didn't get any.")
-                   print("INFO: Waiting for %s secs. before re-polling SQS Queue" % os.environ["WAIT_TIME_FOR_NEXT_POLL"])
-                   time.sleep(int(os.environ["WAIT_TIME_FOR_NEXT_POLL"]))
-            except botocore.exceptions.ClientError as sqs_rec_msg_err:
-                print("ERROR: Issue observed while reading messages from SQS Queue: %s" % str(sqs_rec_msg_err))
-                response = None
-                break
-
-    except Exception as read_err:
-        print("ERROR: Issue observed while reading messages from SQS - %s" % str(read_err))
-
-    return response
+        response = sqsclient.receive_message(
+                                                QueueUrl=os.environ["SQS_URL"],
+                                                AttributeNames=[
+                                                    'All'
+                                                ],
+                                                MaxNumberOfMessages=int(os.environ["COUNT_OF_SQS_READ_MSG"]),
+                                                MessageAttributeNames=[
+                                                    'All'
+                                                ],
+                                                VisibilityTimeout=int(os.environ["SQS_MSG_VISIBILITY_TIMEOUT"])
+                                            )
+        return response
+    except botocore.exceptions.ClientError as sqs_rec_msg_err:
+        print("ERROR: Issue observed while reading messages from SQS Queue: %s" % str(sqs_rec_msg_err))
+        return None
 
 
 def clone_git_repo():
@@ -152,11 +136,16 @@ def clone_git_repo():
             shutil.rmtree(os.environ["CLONED_REPO_DIR"])
         tcp_protocol, git_url = os.environ["CSB_CNT_REPO"].split("//")
         git_repo_url = tcp_protocol + "//" + os.environ["GITHUB_TOKEN_CSBAUDITOR_GEN"] + "@" + git_url
-        git.Repo.clone_from(git_repo_url, os.environ["CLONED_REPO_DIR"])
+        repo = git.Repo.clone_from(git_repo_url, os.environ["CLONED_REPO_DIR"])
+        repo.git.checkout(os.environ["GIT_BRANCH_TO_USE"])
+        root = os.path.expanduser("~")
+        for filename in os.listdir(os.path.join(root, 'csb_cnt_repo')):
+            shutil.move(os.path.join(root, 'csb_cnt_repo', filename), os.path.join(root, filename))
         return True
     except git.exc.GitCommandError as err_clone:
         print("ERROR: Git Clone failed; %s" % str(err_clone))
         return False
+
 
 def process_messages(messages):
     """
@@ -166,7 +155,7 @@ def process_messages(messages):
     """
     print("LOG: Process the messages read from SQS")
     processes = list()
-    try:
+    if "Messages" in messages:
         for message in messages["Messages"]:
             team_name, team_id, test_id, url, scan_id, receipt_handle = retrieve_details(message)
             print("Project: %s, TeamID: %s, TestIDs: %s, URL: %s, Scan ID = %s, Receipt Handle: %s" % (team_name, team_id, test_id, url, scan_id, receipt_handle))
@@ -176,14 +165,17 @@ def process_messages(messages):
                 proc = multiprocessing.Process(target=audit_project, args=(team_id, team_name, test_id, url, scan_id, receipt_handle))
                 processes.append(proc)
                 proc.start()
-
-                for one_process in processes:
-                    one_process.join()
-
             except Exception as e:
-                print("ERROR: Multiprocessing failed for TeamId-TeamName => %s-%s due to %s" % (team_id, team_name, str(e)))
-    except KeyError:
-        print("ERROR: Message received does not hold right set of info")
+                print("ERROR: Message processing failed for TeamId-TeamName => %s-%s due to %s" % (team_id, team_name, str(e)))
+
+        try:
+            for one_process in processes:
+                one_process.join(int(os.environ["MPROC_TIMEOUT"]))
+        except multiprocessing.TimeoutError as proc_err:
+            print("ERROR: Execution of audit_project\(\) for team %s took more time than expected - %s" % (team_name, str(proc_err)))
+
+    else:
+        print("LOG: There was no message available for processing")
 
 
 def retrieve_details(msg):
@@ -193,12 +185,12 @@ def retrieve_details(msg):
     :return: team_name, team_id, test_id, url, scan_id, receipt_handle
     """
     print("LOG: Retrieve individual information available per message")
-    team_name = msg["MessageAttributes"]["teamname"]["StringValue"]
-    test_id = msg["MessageAttributes"]["testid"]["StringValue"]
-    scan_id = msg["MessageAttributes"]["scanid"]["StringValue"]
-    team_id = msg["MessageAttributes"]["teamid"]["StringValue"]
-    url = msg["MessageAttributes"]["url"]["StringValue"]
-    receipt_handle = msg["ReceiptHandle"]
+    team_name = msg["MessageAttributes"]["teamname"].get("StringValue", None)
+    test_id = msg["MessageAttributes"]["testid"].get("StringValue", None)   # [P3|CAE]:<alphanumeric string>
+    scan_id = msg["MessageAttributes"]["scanid"].get("StringValue", None)   # <alphanumeric string>f8a51d2e-1467-11e9-8219-fe4a21889d64
+    team_id = msg["MessageAttributes"]["teamid"].get("StringValue", None)   # [P3|CAE]:<alphanumeric string>
+    url = msg["MessageAttributes"]["url"].get("StringValue", None)          # https...
+    receipt_handle = msg.get("ReceiptHandle", None)
 
     return team_name, team_id, test_id, url, scan_id, receipt_handle
 
@@ -216,56 +208,36 @@ def audit_project(team_id, team_name, test_id, url, scan_id, receipt_handle):
     :return: None
     """
     print("LOG: Execute individual Audit test-scripts per team: %s" % team_name)
-    region = os.environ["SQS_URL"].split(".")[1]
-    session = boto3.session.Session(aws_access_key_id=os.environ["AWS_ACCESS_KEY_ID"], aws_secret_access_key=os.environ["AWS_SECRET_ACCESS_KEY"], region_name=region)
-
-
     audit_test_list = test_id.split(",")
     results = dict()
+    del_flag = True
     for tc in audit_test_list:
         """ Translating TC name to fetch the respective audit script name """
         audit_tc_script = tc.replace("-", "_").lower()
-        print("LOG: Script under execution - %s" % audit_tc_script)
-        """ Update the scan record with \"InProgress\" Status """
-        updateScanRecord(session, team_id.split(":")[0], scan_id, team_id, tc, "InProgress")
 
-        seq_nums_list = []
         if os.path.isfile(audit_tc_script + ".py"):
             tc_script = import_module(audit_tc_script)
-            audit_time = int(time.time()) * 1000
-            counter = 0
             try:
-                results[audit_tc_script] = tc_script.main(url, team_name)
-                params_list = []
-                params = {
-                             "scanid": scan_id,
-                             "testid": tc,
-                             "teamid": str(team_id),
-                             #"teamid-testid-resourceName": "{}-{}-{}".format(str(team_id), tc),
-                             "teamid-testid": "{}-{}".format(str(team_id), tc),
-                             "createdAt": audit_time,
-                             "updatedAt": audit_time,
-#                             "resourceName": item['id'],
-                             "complianceStatus": results[audit_tc_script],
-                           }
-                params_list.append(params.copy())
-
-                print(params_list)
-                while sys.getsizeof(json.dumps(params_list)) >= 900000:
-                    print("LOG: FIRST ELEMENT OF PARAMS LIST: ", params_list[0])
-                    stream_info = add_result_to_stream(session, team_name.split(":")[0], str(team_id), tc, params_list)
-                    seq_nums_list.append(stream_info)
-
-                send_result_complete(session, team_id.split(":")[0], scan_id, team_id, tc, seq_nums_list)
+                results[audit_tc_script] = tc_script.main(url, team_name, scan_id, team_id)
+                if results[audit_tc_script] is None:
+                    print("LOG: Result for %s was received as None; retrying..." % audit_tc_script)
+                    results[audit_tc_script] = tc_script.main(url, team_name, scan_id, team_id)
+                    print("LOG: Second Attempt received result as %" % results[audit_tc_script])
+                    print("LOG: Continuing with next Audit Test-Script")
+                    if results[audit_tc_script] is None:
+                        del_flag = False
             except Exception as e:
-                print("ERROR: Failed read the result from execution => %s" % str(e))
+                del_flag = False
+                print("ERROR: Execution of %s audit test-script didn't return expected value - %s" % (str(e), audit_tc_script))
         else:
             results[audit_tc_script] = "Script is not available"
-            raise Exception("ERROR: Required Test-Script is not available")
+            del_flag = False
+            raise Exception("ERROR: Required Test-Script is not available %s" % audit_tc_script)
     print("LOG: Execution result for Team: %s \n%s" % (team_name, results))
 
+    if del_flag:
+        delete_msg_from_sqs(receipt_handle)
 
-    delete_msg_from_sqs(receipt_handle)
 
 def delete_msg_from_sqs(receipt_handle):
     """
@@ -284,50 +256,66 @@ def delete_msg_from_sqs(receipt_handle):
         print("ERROR: Failed to delete the message from SQS Queue: %s" % str(del_err))
 
 
-def main():
-    """ Key to decrypt the encrypted files required for CSB Audit """
-    #key = os.environ["KEY"]
-    key = "1329ebbc1b9646b890202384beaef2ec"
-
-    """ Decrypt the kube config file """
-    if decrypt_file(key, "kube-config.enc"):
-        print("INFO: Successfully decrypted Kube Config file")
-    else:
-        raise Exception("ERROR: Failed to decrypt \"kube_config.enc\" file")
-
-    """ Decrypt credentials file. Then set environment variables w.r.t. required set of credentials  """
-    if decrypt_file(key, "csb_credentials.py.enc"):
-        print("INFO: Successfully decrypted Credential file")
+def set_credentials_env():
+    """
+    Method to set the environment in terms of credentials to be used during execution
+    :return:
+    """
+    print("LOG: Decrypt credentials file. Then set environment variables w.r.t. required set of credentials")
+    if decrypt_file("csb_credentials.py.enc"):
+        print("LOG: Successfully decrypted Credential file")
         cred_file = import_module("csb_credentials")
         for var, val in cred_file.csb_credentials.items():
             os.environ[var] = val
+        return True
     else:
         raise Exception("ERROR: Failed to decrypt \"csb_credentials.py.enc\" file")
+        return False
 
-    """ Setting environment variables required for execution of CBS-CNT related scripts """
-    print("INFO: Set all required variables as part of ENV")
-    for var, val in env_variables.items():
-        os.environ[var] = val
 
-    """ Clone Git Repo for the audit test-scripts """
-    if clone_git_repo():
-        """ Get the connection handle to AWS SQS """
-        sqsclient = sqs_client_handle()
-        if sqsclient:
-            """ Read messages from AWS SQS """
-            msg_from_sqs = read_message_from_sqs(sqsclient)
-            if msg_from_sqs:
-                """ Initiate processing of the messages received from SQS """
-                process_messages(msg_from_sqs)
+def main():
+    """
+    Method to drive the CSB function of auditing the P3 and CAE Cloud based projects
+    :return:None
+    """
+    if set_credentials_env():
+        """ Decrypt the kube config file """
+        if decrypt_file("kube_config.enc"):
+            print("INFO: Successfully decrypted Kube Config file")
         else:
-            print("ERROR: Failed to get SQS Handle")
+            raise Exception("ERROR: Failed to decrypt \"kube_config.enc\" file")
 
-    """ Delete the decrypted files """
-    print("INFO: Delete decrypted Credential file")
-    os.remove(os.path.expanduser("~") + "/" + "csb_credentials.py")
-    os.remove(os.path.expanduser("~") + "/" + "csb_credentials.pyc")
-    print("INFO: Delete decrypted kube_config file")
-    os.remove(os.path.expanduser("~") + "/" + "kube-config")
+        """ Setting environment variables required for execution of CBS-CNT related scripts """
+        print("INFO: Set all required variables as part of ENV")
+        for var, val in env_variables.items():
+            os.environ[var] = val
+
+        """ Clone Git Repo for the audit test-scripts """
+        if clone_git_repo():
+            """ Get the connection handle to AWS SQS """
+            sqsclient = sqs_client_handle()
+            if sqsclient:
+                """ Read messages from AWS SQS """
+                while True:
+                    msg_from_sqs = read_message_from_sqs(sqsclient)
+                    if msg_from_sqs.get("Messages", None):
+                        print("INFO: Initiate processing of the messages received from SQS")
+                        process_messages(msg_from_sqs)
+                    else:
+                        print("INFO: Attempt to read message from SQS didn't get any.")
+                        print("INFO: Waiting for %s secs. before re-polling SQS Queue" % os.environ["WAIT_TIME_FOR_NEXT_POLL"])
+                        time.sleep(int(os.environ["WAIT_TIME_FOR_NEXT_POLL"]))
+            else:
+                print("ERROR: Failed to get SQS Handle")
+
+        """ Delete the decrypted files """
+        print("INFO: Delete decrypted Credential file")
+        os.remove(os.path.expanduser("~") + "/" + "csb_credentials.py")
+        os.remove(os.path.expanduser("~") + "/" + "csb_credentials.pyc")
+        print("INFO: Delete decrypted kube_config file")
+        os.remove(os.path.expanduser("~") + "/" + "kube_config")
+    else:
+        raise Exception("ERROR: Failed to initialize the environment in terms of credentials to use.")
 
 
 if __name__ == '__main__':
