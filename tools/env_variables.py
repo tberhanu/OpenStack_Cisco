009cf6b376881870dbf97a180cecbc77cd8fc277
amardkum <amardkum@cisco.com>
2019-01-13 05:22:07 -0800

    Update env_variables.py
    Added new variable: AUDIT_SCRIPTS_DIR

--- tools/env_variables.py
+++ tools/env_variables.py
@@ -14,21 +14,25 @@ All rights reserved.
 import os
 
 env_variables = {
+    "AWS_REGION": "us-east-1",
+    "AWS_ENV_TYPE": "dev",
+    "COUNT_OF_SQS_READ_MSG": "1",
     "SQS_URL": "https://sqs.us-east-1.amazonaws.com/283603838660/devCSBRelayQueueCAE.fifo",
     "SQS_MSG_VISIBILITY_TIMEOUT": "60",
+
+    "AUDIT_SCRIPTS_DIR": os.path.expanduser("~") + "/csb_cnt_repo/audit_scripts",
+    "CLONED_REPO_DIR": os.path.expanduser("~") + "/csb_cnt_repo",
     "CSB_CNT_REPO": "https://wwwin-github.cisco.com/CiscoIT-CSB/CiscoIT-CSB",
-    "CLONED_REPO_DIR": os.path.expanduser("~") + "/" + "csb_cnt_repo/",
+    "GIT_BRANCH_TO_USE": "csb_dev",
+
     "OS_INTERFACE": "public",
     "OS_IDENTITY_API_VERSION": "3",
     "OS_PROJECT_DOMAIN_NAME": "cisco",
     "OS_USER_DOMAIN_NAME": "cisco",
-    "MAX_LIMIT_OF_SPAWNED_CONTAINER": "20",
-    "BREAKOUT_HOUR": "10",   # 1000 hrs.
-    "AWS_REGION": "us-east-1",
-    "ENV_TYPE": "",
-    "COUNT_OF_SQS_READ_MSG": "1",
+
+    "MPROC_TIMEOUT": "120",
     "WAIT_TIME_FOR_NEXT_POLL": "60",
-    "WAIT_TIME_FOR_PROJECT_LIST_SCHEDULE": "300"
+    "WAIT_TIME_FOR_PROJECT_LIST_SCHEDULE": "300",
 }
 
 if __name__ == "__main__":
