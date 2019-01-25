544ff0cf5145488bee828cdfdf5b06c43028f07a
ragujja <ragujja@cisco.com>
2019-01-25 14:14:27 -0500

    edited csv path

--- audit_scripts/cae_image_hardening_tc_1.py
+++ audit_scripts/cae_image_hardening_tc_1.py
@@ -36,7 +36,9 @@ session = session_handle()
 
 """ Creating name of CSV file """
 date_stamp = datetime.datetime.now().strftime('%m%d%y')
-csv_filename = os.environ["CLONED_REPO_DIR"] + "/logs/reports/cae_image_hardening_" + date_stamp + "_.csv"
+#csv_filename = os.environ["CLONED_REPO_DIR"] + "/logs/reports/cae_image_hardening_" + date_stamp + "_.csv"
+csv_filename = os.path.expanduser("~") + "/logs/cae_image_hardening_" + date_stamp + ".csv"
+
 
 
 def load_config(path):
