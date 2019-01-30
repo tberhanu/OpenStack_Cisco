1bd4e4272d3abdf06b6940f2ffbe6a52462f02cb
Naga Vijay <nvijayap@gmail.com>
2019-01-29 17:33:43 -0800

    unit_tests/audit_projects.sh has more cp lines

--- unit_tests/audit_projects.sh
+++ unit_tests/audit_projects.sh
@@ -4,7 +4,7 @@
 # audit_projects.sh
 #
 
-TOP=$(cd `dirname $0`; pwd -P); cd $TOP/../tools
+TOOLS_DIR=$(cd `dirname $0`/../tools; pwd -P); cd $TOOLS_DIR
 
 echo
 echo "======================================================================="
@@ -15,7 +15,12 @@ if [ $# -eq 0 ]; then
   echo -e "\nNeed Test_Id(s) as Arg(s). Example Arg: p3_identity_mgmt_tc_1\n"; exit 1
 fi
 
-cp ~/.csb/{csb_credentials.py,csb_credentials.pyc} ~
+cp ~/.csb/csb_credentials.py      $TOOLS_DIR
+cp ~/.csb/csb_credentials.pyc     $TOOLS_DIR
+cp ~/.csb/csb_credentials.py.enc  $TOOLS_DIR
+cp ~/.csb/kube_config_rtp.enc     $TOOLS_DIR
+cp ~/.csb/kube_config_rcdn.enc    $TOOLS_DIR
+cp ~/.csb/kube_config_alln.enc    $TOOLS_DIR
 
 cat >requirements.txt <<!
 gitpython
