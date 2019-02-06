e52fa51371f4fcdd9dcac27b00ed78c94cf059a9
dmalepat <dmalepat@cisco.com>
2019-02-06 10:02:28 -0500

    Update cae_image_hardening_tc_1.py

--- audit_scripts/cae_image_hardening_tc_1.py
+++ audit_scripts/cae_image_hardening_tc_1.py
@@ -36,7 +36,10 @@ session = session_handle()
 
 """ Creating name of CSV file """
 date_stamp = datetime.datetime.now().strftime('%m%d%y')
-csv_filename = os.path.expanduser("~") + "/logs/cae_image_hardening_tc_1_" + date_stamp + ".csv"
+#csv_filename = os.path.expanduser("~") + "/logs/cae_image_hardening_tc_1_" + date_stamp + ".csv"
+csv_filename = "cae_image_hardening_" + date_stamp + "_.csv"
+csv_filename2 = "cae_image_hardening_tc_1_Non_Compliant" + date_stamp + "_.csv"
+#csv_filename2 = os.path.expanduser("~") + "/logs/cae_image_hardening_tc_1_Non_Compliant_" + date_stamp + "_.csv"
 
 
 def load_config(path):
@@ -60,7 +63,7 @@ def get_projects(project_name,path):
     :param path: holds the path to kube config file
     :return: project_file_text
     """
-    project_file_text = []
+    project_file_text = None
     try:
         print("LOG: Inside get_project method to collect info about Project")
         api_handle = load_config(path)
@@ -90,7 +93,6 @@ def get_projects(project_name,path):
             project_file_text = ['None','None', 'None', 'None']
     except Exception as e:
         print("ERROR: Failed to retrieve project list => %s" % str(e))
-
     return project_file_text
 
 
@@ -116,7 +118,7 @@ def get_pods(project_pod,path):
         return None
 
 
-def get_image(project_img, pod, path, compliance_status, flag, scan_id, team_id, scanid_valid, teamid_valid):
+def get_image(project_img, pod, path, compliance_status, scan_id, team_id, scanid_valid, teamid_valid):
     """
     Method to get image details of application running on pod under specified project
     :param project_img: holds Project Name
@@ -125,9 +127,9 @@ def get_image(project_img, pod, path, compliance_status, flag, scan_id, team_id,
     :return: image_data | None
     """
     try:
-        print("LOG: Listing the images in the pods in the project  => %s" % project_img)
         api = load_config(path)
         pod = pykube.Pod.objects(api).filter(namespace=project_img).get(name=pod)
+        print("LOG: Listing the pods in the project  => %s" % pod)
         if pod is not None:
             metadata = pod.obj['metadata']
             pod_name = none_check(metadata.get('name', None))
@@ -148,13 +150,8 @@ def get_image(project_img, pod, path, compliance_status, flag, scan_id, team_id,
                         container_start_date = 'None'
                     for container_list in pod.obj["spec"]["containers"]:
                         image = none_check(container_list.get('image',None))
-                        if re.match('containers.cisco.com\/*', image):
-                            compliance_status = "Compliant"
-                            print("INFO: Secure - %s with image: %s " % (pod, image))
-                        else:
-                            compliance_status = "Non-compliant"
-                            flag = "Non-compliant"
-                            print("INFO: Not Secure! - %s with image: %s " % (pod, image))
+                        print("LOG: Listing the images in the pods in the project  => %s" % image)
+                        compliance_status = compliance_status_validation(image)
                         try:
                             ports = " "
                             for container_port in container_list['ports']:
@@ -176,19 +173,23 @@ def get_image(project_img, pod, path, compliance_status, flag, scan_id, team_id,
                             return None
                     else:
                         print("INFO: ScanId or TeamId passed to main() method is not valid, hence ignoring Kinesis part")
-                    #container_id = none_check(container_id)
-                    #container_start_date = none_check(container_start_date)
                     image_file_text = [pod_name, pod_namespace, pod_status, container_id, image_name, image_id,
                                        container_start_date, ports, compliance_status]
                     proj_txt = get_projects(project_img, path)
-                    image_data = [image_file_text, proj_txt, flag]
-                    build_metadata(image_data)
+                    image_data = [image_file_text, proj_txt, compliance_status]
+                    build_metadata(image_data, csv_filename)
+                    non_compliant_str = 'Non-Compliant'
+                    if compliance_status.lower() == non_compliant_str.lower():
+                        # Write a new CSV file when the image is Non-compliant
+                        create_csv_file_headers(csv_filename2)
+                        build_metadata(image_data, csv_filename2)
+
             else:
                 image_file_text = [pod_name, pod_namespace, pod_status, 'None', 'None', 'None',
                                    'None', 'None', compliance_status]
                 proj_txt = get_projects(project_img, path)
                 image_data = [image_file_text, proj_txt,compliance_status]
-                build_metadata(image_data)
+                build_metadata(image_data, csv_filename)
 
             return image_data
         else:
@@ -199,7 +200,7 @@ def get_image(project_img, pod, path, compliance_status, flag, scan_id, team_id,
         return None
 
 
-def print_metadata(image_file_text, project_file_text):
+def print_metadata(image_file_text, project_file_text, csv_filename):
     """
     Method to print the metadata in image_file_text and Project_file_text
     :param image_file_text: holds pod name,pod status,pod namespace,container id,
@@ -224,10 +225,10 @@ def empty_metadata(namespace, path, compliance_status):
 
     proj_txt = get_projects(namespace, path)
     image_file_text = ["None"] * 8 + [compliance_status]
-    print_metadata(image_file_text, proj_txt)
+    print_metadata(image_file_text, proj_txt, csv_filename)
 
 
-def build_metadata(image_values):
+def build_metadata(image_values, csv_filename):
     """
      Method to build the metadata into a file
     :param namespace: holds name of the project
@@ -240,7 +241,7 @@ def build_metadata(image_values):
         print("LOG: Building the metadata into a file")
         image_file_text = image_values[0]
         project_file_text = image_values[1]
-        print_metadata(image_file_text, project_file_text)
+        print_metadata(image_file_text, project_file_text, csv_filename)
     except Exception as e:
         print("ERROR: Failed to build metadata with error => %s" % str(e))
         return None
@@ -264,6 +265,7 @@ def cae_teamid_validation(team_id):
     else:
         print("ERROR: Received TeamID is not valid")
         return False
+
 
 
 def cae_url_validation(url):
@@ -315,47 +317,37 @@ def main(url, namespace, scan_id, team_id):
                 else:
                     print("INFO: ScanId or TeamId passed to main() method is not valid, hence ignoring Kinesis part")
 
-                if os.path.isfile(csv_filename):
-                    print("INFO: CSV file is available to read")
-                    with open(csv_filename, 'r') as csvFile:
-                        reader = csv.reader(csvFile, delimiter=",")
-                        data = list(reader)
-                        row_count = len(data)
-                else:
-                    print("INFO: CSV file is not available to read, writing new file ")
-                    row_count = 0
-
-                file_headers = ['Tenant Name', 'Tenant ID', 'Application ID', 'Application Name', 'Pod Name',
-                                'Pod Namespace', 'Pod Status', 'Container ID', 'Image Name', 'Image ID',
-                                'Container Start Date', 'Container Exposed Port ','Compliance_Status'
-                               ]
-                file_content = [file_headers]
-                if row_count <= 0:
-                    with open(csv_filename, 'a') as csvFile:
-                        writer = csv.writer(csvFile)
-                        writer.writerows(file_content)
+                create_csv_file_headers(csv_filename)
                 print("LOG: Check whether source of image used is a trusted one")
                 pods_list = get_pods(namespace, path)
-                get_projects(namespace, path)
+                proj_txt = get_projects(namespace, path)
                 compliance_status = "Compliant"
-                if pods_list is not None:
-                    for pod in pods_list:
-                        image_data = get_image(namespace, pod, path, compliance_status, flag, scan_id, team_id, scanid_valid, teamid_valid)
-                        flag = image_data[2]
-                else:
-                    print("INFO: No Pods running in the project")
-                    empty_metadata(namespace, path, compliance_status)
-                    params_list = []
-                    pod = 'NULL'
-                    if scanid_valid and teamid_valid:
-                        if kinesis_update(session,"CAE", scan_id, tc, team_id, pod, compliance_status,params_list):
-                            print("LOG: Added the info to Kinesis Stream")
-                        else:
-                            print("LOG: Kinesis Update API Failed")
-                            return None
+                non_compliant_str = 'Non-Compliant'
+                if proj_txt is not None:
+                    if pods_list is not None:
+                        for pod in pods_list:
+                            image_data = get_image(namespace, pod, path, compliance_status, scan_id, team_id, scanid_valid, teamid_valid)
+                            flag_txt = image_data[2]
+                            if flag_txt.lower() == non_compliant_str.lower():
+                                flag = non_compliant_str
                     else:
-                        print("INFO: ScanId or TeamId passed to main() method is not valid, hence ignoring Kinesis part")
-                    return compliance_status
+                        print("INFO: No Pods running in the project")
+                        empty_metadata(namespace, path, compliance_status)
+                        params_list = []
+                        pod = 'NULL'
+                        if scanid_valid and teamid_valid:
+                            if kinesis_update(session,"CAE", scan_id, tc, team_id, pod, compliance_status,params_list):
+                                print("LOG: Added the info to Kinesis Stream")
+                            else:
+                                print("LOG: Kinesis Update API Failed")
+                                return None
+                        else:
+                            print("INFO: ScanId or TeamId passed to main() method is not valid, hence ignoring Kinesis part")
+                        return compliance_status
+                else:
+                    proj_txt = [namespace] + ["Does not exist"] * 4
+                    image_file_text = ["Does not exist"] * 9
+                    print_metadata(image_file_text, proj_txt, csv_filename)
         else:
             print("ERROR:URL not found")
             return None
@@ -419,6 +411,36 @@ def none_check(val):
     if val is None:
         val = 'None'
     return val
+
+
+def create_csv_file_headers(csv_filename):
+    if os.path.isfile(csv_filename):
+        print("INFO: CSV file is available to read")
+        with open(csv_filename, 'r') as csvFile:
+            reader = csv.reader(csvFile, delimiter=",")
+            data = list(reader)
+            row_count = len(data)
+    else:
+        print("INFO: CSV file is not available to read, writing new file ")
+        row_count = 0
+    if row_count <= 0:
+        file_headers = ['Tenant Name', 'Tenant ID', 'Application ID', 'Application Name', 'Pod Name',
+                        'Pod Namespace', 'Pod Status', 'Container ID', 'Image Name', 'Image ID',
+                        'Container Start Date', 'Container Exposed Port ', 'Compliance_Status'
+                        ]
+        file_content = [file_headers]
+        with open(csv_filename, 'a') as csvFile:
+            writer = csv.writer(csvFile)
+            writer.writerows(file_content)
+
+
+def compliance_status_validation(image):
+    if re.match(r'^containers.*.cisco.com\/*', image):
+        compliance_status = "Compliant"
+        return compliance_status
+    else:
+        compliance_status = "Non-Compliant"
+        return compliance_status
 
 
 if __name__ == '__main__':
