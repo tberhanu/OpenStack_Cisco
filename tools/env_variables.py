5659ee6dedfd4d6f0845c9f940020006e7b77829
ragujja <ragujja@cisco.com>
2019-02-06 11:57:51 -0500

    updated aws api url

--- tools/env_variables.py
+++ tools/env_variables.py
@@ -17,15 +17,16 @@ env_variables = {
     "AWS_REGION": "us-east-1",
     "AWS_SERVICE": "execute-api",
     "AWS_ENV_TYPE": "dev",
-    "AWS_EXECUTE_API_URL": "https://hcbfujwsma.execute-api.us-east-1.amazonaws.com/abhishupStaging/teams",
-    "AWS_CANONICAL_URI": "/abhishupStaging/teams",
-    "AWS_API_KEY": "UkK1xt9IxK6fANKnJ1PeF7ev2w4hQ2C666aeRksq",
+    "AWS_EXECUTE_API_URL" : "https://n98uo7cw6i.execute-api.us-east-1.amazonaws.com/devStaging/teams",
+    "AWS_CANONICAL_URI": "/devStaging/teams",
+    "AWS_API_KEY":"o69aEfEEO54JLbkODkVE1SE1g6tXkBLaW8B7FnFb",
     "COUNT_OF_SQS_READ_MSG": "1",
     "SQS_URL": "https://sqs.us-east-1.amazonaws.com/283603838660/devCSBRelayQueueCAE.fifo",
     "SQS_MSG_VISIBILITY_TIMEOUT": "60",
 
     "AUDIT_SCRIPTS_DIR": os.path.expanduser("~") + "/csb_cnt_repo/audit_scripts",
     "CLONED_REPO_DIR": os.path.expanduser("~") + "/csb_cnt_repo",
+    "LOGS_DIR": os.path.expanduser("~") + "/logs",
     "CSB_CNT_REPO": "https://wwwin-github.cisco.com/CiscoIT-CSB/CiscoIT-CSB",
     "GIT_BRANCH_TO_USE": "csb_dev",
 
@@ -34,9 +35,9 @@ env_variables = {
     "OS_PROJECT_DOMAIN_NAME": "cisco",
     "OS_USER_DOMAIN_NAME": "cisco",
 
-    "MPROC_TIMEOUT": "300",
+    "MPROC_TIMEOUT": "120",
     "WAIT_TIME_FOR_NEXT_POLL": "60",
-    "WAIT_TIME_FOR_PROJECT_LIST_SCHEDULE": "3600",
+    "WAIT_TIME_FOR_PROJECT_LIST_SCHEDULE": "86400",
 }
 
 if __name__ == "__main__":
