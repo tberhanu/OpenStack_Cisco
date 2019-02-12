590307a24702a93a2d89e6d97d5b9390d64be88a
devaacha <devaacha@cisco.com>
2019-02-12 13:50:14 -0500

    'update for the common api used in the platform'

---
+++ library/cae_lib.py
@@ -0,0 +1,77 @@
+#!/opt/app-root/bin/python
+
+"""
+------------------------------- cae_lib.py ----------------------------
+Description: This python module holds the APIs required for CAE
+             platform related operations.
+
+Author: Devaraj Acharya <devaccha@cisco.com>; February 12th, 2019
+
+Copyright (c) 2019 Cisco Systems.
+All rights reserved.
+-----------------------------------------------------------------------
+"""
+
+import os
+import pykube
+import re
+
+
+def load_config(url):
+    """
+    Method to create API Handle
+    :param path: holds the path to kube config file
+    :return: api handle
+    """
+    try:
+        if url is not None:
+            print("---->url",url)
+            region_name = url.split(".")[0].split("-")[-1]
+            print("---->region name",region_name)
+            if region_name is not None:
+                path = os.path.expanduser("~") + "/" + "kube_config_" + region_name
+                print("---->path name", path)
+            else:
+                raise Exception("ERROR: Region name is None")
+            api = pykube.HTTPClient(pykube.KubeConfig.from_file(path))
+            return api
+        else:
+            print("ERROR: URL not found")
+    except Exception as e:
+        print("ERROR: Failed to retrieve pykube.KubeConfig.from_file path => %s" % str(e))
+        return None
+
+
+def cae_teamid_validation(team_id):
+    """
+    Method to validate the recevied teamId for a CAE Tenant
+    :param team_id:
+    :return:
+    """
+    teamid_pattern = re.compile(r'^CAE:[0-9a-f]{8}-[0-9a-f]{4}-[0-9a-f]{4}-[0-9a-f]{4}-[0-9a-f]{12}$')
+    if teamid_pattern.match(team_id):
+        print("LOG: Received valid TeamID")
+        return True
+    else:
+        print("ERROR: Received TeamID is not valid")
+        return False
+
+
+def cae_url_validation(url):
+    """
+    Method to validate the received domain url
+    :param url: Domain URL
+    :return: True or False
+    """
+    cae_url_pattern = re.compile(r'^https://cae-np-.*.cisco.com$')
+    if cae_url_pattern.match(url):
+        print("LOG: Received valid Domain URL")
+        return True
+    else:
+        print("ERROR: Received Domain URL is not valid")
+        return False
+
+
+
+
+
