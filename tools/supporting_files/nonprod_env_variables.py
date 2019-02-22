27972cf8a77ec42f1e051aa54688c76556055387
amardkum <amardkum@cisco.com>
2019-02-21 18:28:33 -0800

    Update and rename tools/env_variables.py to tools/supporting_files/nonprod_env_variables.py

--- tools/supporting_files/nonprod_env_variables.py
+++ tools/supporting_files/nonprod_env_variables.py
@@ -1,9 +1,9 @@
 #!/opt/app-root/bin/python
 
 """
-------------------------------- env_variables.py ------------------------------
-Description : This file is meant to maintain a dictionary of env. variables
-              along with their values as key-value pairs.
+---------------------------- nonprod_env_variables.py -------------------------
+Description : This file is meant to maintain a dictionary of nonprod env.
+              related variables along with their values as key-value pairs.
 
 Author: Amardeep Kumar <amardkum@cisco.com>; January 3rd, 2019
 
@@ -13,18 +13,19 @@ All rights reserved.
 """
 import os
 
-env_variables = {
+nonprod_env_variables = {
     "AWS_REGION": "us-east-1",
     "AWS_SERVICE": "execute-api",
     "AWS_ENV_TYPE": "dev",
     "AWS_EXECUTE_API_URL": "https://n98uo7cw6i.execute-api.us-east-1.amazonaws.com/devStaging/teams",
     "AWS_CANONICAL_URI": "/devStaging/teams",
-    "COUNT_OF_SQS_READ_MSG": "1",
     "SQS_URL": "https://sqs.us-east-1.amazonaws.com/283603838660/devCSBRelayQueueCAE",
     "SQS_DEAD_LETTER_QUEUE_URL": "https://sqs.us-east-1.amazonaws.com/283603838660/devCSBDeadLetterQueueCAE",
     "SQS_MSG_POLL_TIME": "15",
     "SQS_MSG_VISIBILITY_TIMEOUT": "600",
-
+    "COUNT_OF_SQS_READ_MSG": "1",
+    "COUNT_OF_DEAD_LETTER_QUEUE_MSG": "10",
+  
     "AUDIT_SCRIPTS_DIR": os.path.expanduser("~") + "/csb_cnt_repo/audit_scripts",
     "CLONED_REPO_DIR": os.path.expanduser("~") + "/csb_cnt_repo",
     "LOGS_DIR": os.path.expanduser("~") + "/logs",
@@ -42,5 +43,5 @@ env_variables = {
 }
 
 if __name__ == "__main__":
-    print(env_variables)
+    print(nonprod_env_variables)
     pass
