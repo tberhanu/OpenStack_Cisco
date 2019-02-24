7a6858aa239871a8f24e66cbdf572b953519c93f
amardkum <amardkum@cisco.com>
2019-02-23 19:23:46 -0800

    Update cae_lib.py

--- library/cae_lib.py
+++ library/cae_lib.py
@@ -12,9 +12,15 @@ All rights reserved.
 -----------------------------------------------------------------------
 """
 
+import errno
 import os
 import pykube
 import re
+import subprocess
+import sys
+
+sys.path.append(os.environ["CLONED_REPO_DIR"] + "/tools/supporting_files")
+from landscape_of_execution import landscape
 
 
 def get_kube_config_path(url):
@@ -47,7 +53,7 @@ def load_config(url):
 
 def cae_teamid_validation(team_id):
     """
-    Method to validate the recevied teamId for a CAE Tenant
+    Method to validate the received teamId for a CAE Tenant
     :param team_id:
     :return:
     """
@@ -73,3 +79,51 @@ def cae_url_validation(url):
     else:
         print("ERROR: Received Domain URL is not valid")
         return False
+
+
+def generate_kube_config_file():
+    """
+    Method to generate Kube config file per CAE Cluster listed in landscape_of_execution.py file
+    :return: True|False
+    """
+    kube_config_at_home = os.path.expanduser("~") + "/.kube/config"
+    for cluster in landscape["CAE_CLUSTER"]:
+        try:
+            os.remove(kube_config_at_home)
+        except OSError as rm_err:
+            if rm_err.errno == errno.ENOENT:
+                print("INFO: %s" % str(rm_err))
+            else:
+                print("ERROR: %s" % str(rm_err))
+                return False
+
+        print("INFO: Cluster - %s" % cluster)
+        out = subprocess.Popen([
+                                '/usr/bin/oc', 'login', cluster,
+                                '-u', os.environ["OC_USERNAME"],
+                                '-p', os.environ["OC_PASSWORD"],
+                                '--insecure-skip-tls-verify'
+                                ],
+                               stdout=subprocess.PIPE,
+                               stderr=subprocess.STDOUT
+                              )
+        stdout, stderr = out.communicate()
+        print("DEBUG: STDOUT while logging into Cluster: %s - %s" % (cluster, stdout))
+        if stderr:
+            print("ERROR: stderr while logging in to Cluster: %s - %s" % (cluster, stderr))
+            return False
+
+        if "Login successful" in str(stdout):
+            kube_config_rgn = get_kube_config_path(cluster)
+            out = subprocess.Popen(['cp', kube_config_at_home, kube_config_rgn],
+                                   stdout=subprocess.PIPE, stderr=subprocess.STDOUT)
+            stdout, stderr = out.communicate()
+            if stderr:
+                print("ERROR: STDERR while copying the file - %s" % stderr)
+                return False
+            elif os.path.isfile(kube_config_rgn):
+                print("INFO: Generated Kube config file - %s" % kube_config_rgn)
+        else:
+            print("ERROR: Failed to login to Cluster: %s" % cluster)
+            return False
+    return True
