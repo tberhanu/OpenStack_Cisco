8e9f64b4519480ad074b72930cf0c830e00fbd8f
ragujja <ragujja@cisco.com>
2019-03-04 16:59:49 -0500

    Add files via upload

--- tools/shell_scripts/teamsdb.sh
+++ tools/shell_scripts/teamsdb.sh
@@ -16,7 +16,7 @@ mkdir -p $CSB_AUDITOR/prod
 #for prod
 cd $TEAMSDB/prod
 cp $tools_dir/{p3_cae_list_projects_and_update_dynamoDB.py,teams_db_update_util.py,audit_projects.py} .
-cp $tools_dir/supporting_files/{audit_tc_list.py,landscape_of_execution.py,requirements.txt,data_cae.xml,data_p3.xml,prod_env_variables.py,nonprod_env_variables.py} .
+cp $tools_dir/supporting_files/{audit_tc_list.py,landscape_of_execution.py,requirements.txt,data_cae.xml,data_cae_vm.xml,data_p3.xml,prod_env_variables.py,nonprod_env_variables.py} .
 cp $tools_dir/docker_files/prod_teamsDb_Dockerfile .
 mv prod_teamsDb_Dockerfile ./Dockerfile
 cp $tools_dir/yaml_files/prod_teamsdbupdate.yaml .
@@ -26,7 +26,7 @@ cd $WORKING_DIR
 cd $RELEASE_DIR
 cd $TEAMSDB/nonprod
 cp $tools_dir/{p3_cae_list_projects_and_update_dynamoDB.py,teams_db_update_util.py,audit_projects.py} .
-cp $tools_dir/supporting_files/{audit_tc_list.py,landscape_of_execution.py,requirements.txt,data_cae.xml,data_p3.xml,prod_env_variables.py,nonprod_env_variables.py} .
+cp $tools_dir/supporting_files/{audit_tc_list.py,landscape_of_execution.py,requirements.txt,data_cae.xml,data_cae_vm.xml,data_p3.xml,prod_env_variables.py,nonprod_env_variables.py} .
 cp $tools_dir/docker_files/nonprod_teamsdb_Dockerfile .
 mv nonprod_teamsdb_Dockerfile ./Dockerfile
 cp $tools_dir/yaml_files/nonprod_teamsdbupdate.yaml .
